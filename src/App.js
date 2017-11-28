465c4e762e98ce37c19a7578fc60fc0808d081e5
Chen Fan <vincebox9111@gmail.com>
2017-11-27 20:24:42 -0500

    init project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+
+// Components:
+import Login from './components/views/public/Login'
+import Signup from './components/views/public/Signup'
+import Home from './components/views/Home'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/home" component={Home}></Route>
+                        <Route path="/signup" component={Signup}></Route>
+                        <Route path="/" component={Login}></Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default App
